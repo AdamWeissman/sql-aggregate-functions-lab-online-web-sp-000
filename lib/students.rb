45d5095408c612491a217eb470739019c8b0ced9
Adam Weissman <adam@comedytragedyepic.com>
2019-11-07 10:23:20 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,19 +1,19 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM students;"
+  "SELECT MAX(gpa) AS the_genius FROM students;"
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) FROM students;"
+  "SELECT MIN(gpa) AS the_dummy FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM students;"
+  "SELECT AVG(gpa) AS typical FROM students;"
 end
 
 def total_tardies_for_all_students
-  "SELECT SUM(tardies) FROM students;"
+  "SELECT SUM(tardies) AS might_key_your_car_professor FROM students;"
 end
 
 def average_gpa_for_9th_grade
